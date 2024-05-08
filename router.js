0400fe2262fc35eba85e54ef2ea830deebb7abe5
Hun <dudgns9194@gmail.com>
2024-05-08 15:22:22 +0900

    productId 매개변수 삭제

--- router.js
+++ router.js
@@ -1,8 +1,8 @@
-function route(pathname, handle, response, productId) {
+function route(pathname, handle, response) {
     console.log('pathname : ' + pathname);
 
     if (typeof handle[pathname] === 'function') {
-        handle[pathname](response, productId);
+        handle[pathname](response);
     } else {
         response.writeHead(404, { 'Content-Type': 'text/html; charset=utf-8' });
         response.write('찾으시는 페이지가 없습니다.');
--- server.js
+++ server.js
@@ -4,9 +4,8 @@ let url = require('url');
 function start(route, handle){
     function onRequest(request, response){
         let pathname = url.parse(request.url).pathname;
-        let queryData = url.parse(request.url, true).query;
 
-        route(pathname, handle, response, queryData.productId);
+        route(pathname, handle, response);
 
     }
     
