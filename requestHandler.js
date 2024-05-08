39102ea8f3b0ad8ce18ad57d9a5856bc33359a8b
Hun <dudgns9194@gmail.com>
2024-05-08 13:48:40 +0900

    공부 및 프로젝트 정리 제작

--- requestHandler.js
+++ requestHandler.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const main_view = fs.readFileSync('./main.html', 'utf-8');
 const orderlist_view = fs.readFileSync('./orderlist.html', 'utf-8');
 const tennisMarket_view = fs.readFileSync('./tennisMarket.html', 'utf-8');
+const projectLogList_view = fs.readFileSync('./projectLogList.html', 'utf-8');
 
 const mariadb = require('./database/connect/mariadb');
 
@@ -14,6 +15,20 @@ function main(response) {
 
     response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
     response.write(main_view);
+    response.end();
+}
+
+function tennis(response) {
+    fs.readFile('./img/tennisMarket.jpg', function (err, data) {
+        response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+        response.write(data);
+        response.end();
+    })
+}
+
+function tennisMarket(response){
+    response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    response.write(tennisMarket_view);
     response.end();
 }
 
@@ -72,18 +87,18 @@ function orderlist(response) {
     })
 }
 
-function tennis(response) {
-    fs.readFile('./img/tennisMarket.jpg', function (err, data) {
+function projectLoglist(response){
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(projectLogList_view);
+    response.end();
+}
+
+function proejctImg(response){
+    fs.readFile('./img/projectLog.jpg', function (err, data) {
         response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
         response.write(data);
         response.end();
     })
-}
-
-function tennisMarket(response){
-    response.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-    response.write(tennisMarket_view);
-    response.end();
 }
 
 let handle = {};
@@ -91,9 +106,11 @@ handle['/'] = main;
 handle['/tennisMarket'] = tennisMarket;
 handle['/tennisMarket/order'] = order;
 handle['/tennisMarket/orderlist'] = orderlist;
+handle['/projectLogList'] = projectLoglist;
 
 
 handle['/img/tennisMarket.jpg'] = tennis;
+handle['/img/projectLog.jpg'] = proejctImg;
 handle['/img/redRacket.png'] = redRacket;
 handle['/img/blueRacket.png'] = blueRacket;
 handle['/img/blackRacket.png'] = blackRacket;
